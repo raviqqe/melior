66117d4d5b4b5ad90601f86151e3f59333e29ea2
Yota Toyama <raviqqe@gmail.com>
2022-09-23 20:55:01 -0700

    Update dependencies (#71)
    - Update dependencies
    - Update setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,14 +2,9 @@
 
 set -e
 
-# TODO Use Homebrew to install LLVM 15.
-# llvm_version=15
+llvm_version=15
 
-# brew update
-# brew install llvm@$llvm_version
+brew update
+brew install llvm@$llvm_version
 
-# echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
-
-curl -fsSL https://apt.llvm.org/llvm.sh | sudo bash -s 15 all
-sudo apt -y install libmlir-15-dev mlir-15-tools
-echo PATH=/usr/lib/llvm-15/bin:$PATH >>$GITHUB_ENV
+echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
