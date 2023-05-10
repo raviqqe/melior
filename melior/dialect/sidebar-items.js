22e8685bddd60d9e9f1c392ea8ddc7e12fd4541f
raviqqe <raviqqe@users.noreply.github.com>
2023-05-10 09:43:42 +0000

    deploy: 944eebad24b3d0abdcc0a1214ec9c39c7cd9b41f

--- melior/dialect/sidebar-items.js
+++ melior/dialect/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["arith","func","llvm","scf"],"struct":["Dialect","DialectHandle","DialectRegistry"]};
+window.SIDEBAR_ITEMS = {"mod":["arith","func","index","llvm","scf"],"struct":["Dialect","DialectHandle","DialectRegistry"]};
