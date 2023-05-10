2addef55b8c9650cff49696a27b78fd62c985e0c
raviqqe <raviqqe@users.noreply.github.com>
2023-05-10 10:55:33 +0000

    deploy: 0acc0c380dd31952d0148fc9b0643d5bf091649e

--- melior/dialect/index/sidebar-items.js
+++ melior/dialect/index/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["add","and","casts","castu","ceildivs","ceildivu","cmp","constant","divs","divu","floordivs","maxs","maxu","mins","minu","mul","or","rems","remu","shl","shrs","shrui","sub","xor"]};
+window.SIDEBAR_ITEMS = {"fn":["add","and","casts","castu","ceildivs","ceildivu","cmp","constant","divs","divu","floordivs","maxs","maxu","mins","minu","mul","or","rems","remu","shl","shrs","shru","sub","xor"]};
