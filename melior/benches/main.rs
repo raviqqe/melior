ca6027968158f586257435df68911907c7d8d370
Yota Toyama <raviqqe@gmail.com>
2023-05-11 13:39:41 +1000

    Add benchmark for cached string ref creation (#161)

--- melior/benches/main.rs
+++ melior/benches/main.rs
@@ -1,8 +1,12 @@
 use criterion::{criterion_group, criterion_main, Bencher, Criterion};
 use melior::StringRef;
 
+const ITERATION_COUNT: usize = 1000000;
+
 fn generate_strings() -> Vec<String> {
-    (0..1000000).map(|number| number.to_string()).collect()
+    (0..ITERATION_COUNT)
+        .map(|number| number.to_string())
+        .collect()
 }
 
 fn string_ref_create(bencher: &mut Bencher) {
@@ -15,8 +19,17 @@ fn string_ref_create(bencher: &mut Bencher) {
     });
 }
 
+fn string_ref_create_cached(bencher: &mut Bencher) {
+    bencher.iter(|| {
+        for _ in 0..ITERATION_COUNT {
+            let _ = StringRef::from("foo");
+        }
+    });
+}
+
 fn benchmark(criterion: &mut Criterion) {
     criterion.bench_function("string ref create", string_ref_create);
+    criterion.bench_function("string ref create cached", string_ref_create_cached);
 }
 
 criterion_group!(benchmark_group, benchmark);
