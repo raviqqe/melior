76a0464ff6f0a171629d01169cb086dd6a353dfb
Yota Toyama <raviqqe@gmail.com>
2023-07-15 20:05:38 +1000

    Fix macOS CI (#250)

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,6 +4,10 @@ set -e
 
 llvm_version=16
 
+if [ -n "$CI" ]; then
+  brew install --overwrite python@3.11
+fi
+
 brew install llvm@$llvm_version
 
 echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
