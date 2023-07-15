2c956b976d82180acecfb307d527455def839afd
raviqqe <raviqqe@users.noreply.github.com>
2023-07-15 09:55:51 +0000

    deploy: 018643429a3e290437f8e4c19f943f4440098dc4

--- melior/dialect/llvm/sidebar-items.js
+++ melior/dialect/llvm/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["extract_value","func","get_element_ptr","get_element_ptr_dynamic","insert_value","load","poison","return","store","undef"],"mod":["attributes","type"],"struct":["LoadStoreOptions"]};
+window.SIDEBAR_ITEMS = {"fn":["alloca","bitcast","call_intrinsic","extract_value","func","get_element_ptr","get_element_ptr_dynamic","insert_value","load","nullptr","poison","return","store","undef","unreachable"],"mod":["attributes","type"],"struct":["AllocaOptions","LoadStoreOptions"]};
