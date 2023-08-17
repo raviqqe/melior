5d1a29910240671a867a39adc750d95935a06d8c
Yota Toyama <raviqqe@gmail.com>
2023-08-17 19:11:58 +1000

    Refactor (#282)

--- macro/src/dialect/operation/accessors.rs
+++ macro/src/dialect/operation/accessors.rs
@@ -1,5 +1,4 @@
 use super::{FieldKind, OperationField, SequenceInfo, VariadicKind};
-
 use crate::utility::sanitize_name_snake;
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
@@ -7,7 +6,7 @@ use quote::{format_ident, quote};
 impl<'a> OperationField<'a> {
     fn getter_impl(&self) -> Option<TokenStream> {
         match &self.kind {
-            FieldKind::Operand(t) | FieldKind::Result(t) => {
+            FieldKind::Operand(constraint) | FieldKind::Result(constraint) => {
                 let kind = self.kind.as_str();
                 let kind_ident = format_ident!("{}", kind);
                 let plural = format_ident!("{}s", kind);
@@ -20,13 +19,14 @@ impl<'a> OperationField<'a> {
                     .variadic_info
                     .as_ref()
                     .expect("operands and results need variadic info");
+
                 Some(match variadic_kind {
                     VariadicKind::Simple {
                         seen_variable_length,
                     } => {
                         // At most one variable length group
-                        if t.is_variable_length() {
-                            if t.is_optional() {
+                        if constraint.is_variable_length() {
+                            if constraint.is_optional() {
                                 // Optional element, and some singular elements.
                                 // Only present if the amount of groups is at least the number of
                                 // elements.
@@ -73,8 +73,7 @@ impl<'a> OperationField<'a> {
                             let group_len = total_var_len / #num_variable_length;
                             let start = #num_preceding_simple + #num_preceding_variadic * group_len;
                         };
-
-                        let get_elements = if t.is_variable_length() {
+                        let get_elements = if constraint.is_variable_length() {
                             quote! {
                                 self.operation.#plural().skip(start).take(group_len)
                             }
@@ -83,56 +82,58 @@ impl<'a> OperationField<'a> {
                                 self.operation.#kind_ident(start).expect(#error)
                             }
                         };
+
                         quote! { #compute_start_length #get_elements }
                     }
                     VariadicKind::AttrSized {} => {
                         let error = format!("operation should have this {}", kind);
-                        let attr_name = format!("{}_segment_sizes", kind);
-                        let attr_missing_error = format!("operation has {} attribute", attr_name);
+                        let attribute_name = format!("{}_segment_sizes", kind);
+                        let attribute_missing_error =
+                            format!("operation has {} attribute", attribute_name);
                         let compute_start_length = quote! {
                             let attribute =
                                 ::melior::ir::attribute::DenseI32ArrayAttribute::<'c>::try_from(
                                     self.operation
-                                        .attribute(#attr_name)
-                                        .expect(#attr_missing_error)
+                                        .attribute(#attribute_name)
+                                        .expect(#attribute_missing_error)
                                 ).expect("is a DenseI32ArrayAttribute");
                             let start = (0..#index)
-                                            .map(|i| attribute.element(i)
-                                            .expect("has segment size"))
-                                            .sum::<i32>() as usize;
+                                .map(|index| attribute.element(index)
+                                .expect("has segment size"))
+                                .sum::<i32>() as usize;
                             let group_len = attribute
-                                                .element(#index)
-                                                .expect("has segment size") as usize;
+                                .element(#index)
+                                .expect("has segment size") as usize;
                         };
-                        let get_elements = if t.is_variable_length() {
-                            if t.is_optional() {
-                                quote! {
-                                    if group_len == 0 {
-                                        None
-                                    } else {
-                                        self.operation.#kind_ident(start).ok()
-                                    }
-                                }
-                            } else {
-                                quote! {
-                                    self.operation.#plural().skip(start).take(group_len)
+                        let get_elements = if !constraint.is_variable_length() {
+                            quote! {
+                                self.operation.#kind_ident(start).expect(#error)
+                            }
+                        } else if constraint.is_optional() {
+                            quote! {
+                                if group_len == 0 {
+                                    None
+                                } else {
+                                    self.operation.#kind_ident(start).ok()
                                 }
                             }
                         } else {
                             quote! {
-                                self.operation.#kind_ident(start).expect(#error)
+                                self.operation.#plural().skip(start).take(group_len)
                             }
                         };
+
                         quote! { #compute_start_length #get_elements }
                     }
                 })
             }
-            FieldKind::Successor(s) => {
+            FieldKind::Successor(constraint) => {
                 let SequenceInfo { index, .. } = self
                     .seq_info
                     .as_ref()
                     .expect("successors need sequence info");
-                Some(if s.is_variadic() {
+
+                Some(if constraint.is_variadic() {
                     // Only the last successor can be variadic
                     quote! {
                         self.operation.successors().skip(#index)
@@ -143,10 +144,11 @@ impl<'a> OperationField<'a> {
                     }
                 })
             }
-            FieldKind::Region(r) => {
+            FieldKind::Region(constraint) => {
                 let SequenceInfo { index, .. } =
                     self.seq_info.as_ref().expect("regions need sequence info");
-                Some(if r.is_variadic() {
+
+                Some(if constraint.is_variadic() {
                     // Only the last region can be variadic
                     quote! {
                         self.operation.regions().skip(#index)
@@ -157,23 +159,24 @@ impl<'a> OperationField<'a> {
                     }
                 })
             }
-            FieldKind::Attribute(a) => {
-                let n = &self.name;
-                let attr_error = format!("operation should have attribute {}", n);
-                let type_error = format!("{} should be a {}", n, a.storage_type());
-                Some(if a.is_unit() {
-                    quote! { self.operation.attribute(#n).is_some() }
-                } else if a.is_optional() {
+            FieldKind::Attribute(constraint) => {
+                let name = &self.name;
+                let attribute_error = format!("operation should have attribute {}", name);
+                let type_error = format!("{} should be a {}", name, constraint.storage_type());
+
+                Some(if constraint.is_unit() {
+                    quote! { self.operation.attribute(#name).is_some() }
+                } else if constraint.is_optional() {
                     quote! {
                         self.operation
-                            .attribute(#n)
-                            .map(|a| a.try_into().expect(#type_error))
+                            .attribute(#name)
+                            .map(|attribute| attribute.try_into().expect(#type_error))
                     }
                 } else {
                     quote! {
                         self.operation
-                            .attribute(#n)
-                            .expect(#attr_error)
+                            .attribute(#name)
+                            .expect(#attribute_error)
                             .try_into()
                             .expect(#type_error)
                     }
@@ -184,12 +187,12 @@ impl<'a> OperationField<'a> {
 
     fn remover_impl(&self) -> Option<TokenStream> {
         match &self.kind {
-            FieldKind::Attribute(a) => {
-                let n = &self.name;
+            FieldKind::Attribute(constraint) => {
+                let name = &self.name;
 
-                if a.is_unit() || a.is_optional() {
+                if constraint.is_unit() || constraint.is_optional() {
                     Some(quote! {
-                      let _ = self.operation.remove_attribute(#n);
+                      let _ = self.operation.remove_attribute(#name);
                     })
                 } else {
                     None
@@ -200,57 +203,55 @@ impl<'a> OperationField<'a> {
     }
 
     fn setter_impl(&self) -> Option<TokenStream> {
-        match &self.kind {
-            FieldKind::Attribute(a) => {
-                let n = &self.name;
+        let FieldKind::Attribute(constraint) = &self.kind else {
+            return None;
+        };
+        let name = &self.name;
 
-                Some(if a.is_unit() {
-                    quote! {
-                        if value {
-                          self.operation.set_attribute(#n, Attribute::unit(&self.operation.context()));
-                        } else {
-                          let _ = self.operation.remove_attribute(#n);
-                        }
-                    }
+        Some(if constraint.is_unit() {
+            quote! {
+                if value {
+                  self.operation.set_attribute(#name, Attribute::unit(&self.operation.context()));
                 } else {
-                    quote! {
-                        self.operation.set_attribute(#n, &value.into());
-                    }
-                })
+                  let _ = self.operation.remove_attribute(#name);
+                }
             }
-            _ => None,
-        }
+        } else {
+            quote! {
+                self.operation.set_attribute(#name, &value.into());
+            }
+        })
     }
 
     pub fn accessors(&self) -> TokenStream {
         let setter = {
-            let set_fn_ident = sanitize_name_snake(&format!("set_{}", self.name));
-            self.setter_impl().map_or(quote!(), |imp| {
+            let ident = sanitize_name_snake(&format!("set_{}", self.name));
+            self.setter_impl().map_or(quote!(), |body| {
                 let param_type = &self.param_type;
                 quote! {
-                    pub fn #set_fn_ident(&mut self, value: #param_type) {
-                        #imp
+                    pub fn #ident(&mut self, value: #param_type) {
+                        #body
                     }
                 }
             })
         };
         let remover = {
-            let remove_fn_ident = sanitize_name_snake(&format!("remove_{}", self.name));
-            self.remover_impl().map_or(quote!(), |imp| {
+            let ident = sanitize_name_snake(&format!("remove_{}", self.name));
+            self.remover_impl().map_or(quote!(), |body| {
                 quote! {
-                    pub fn #remove_fn_ident(&mut self) {
-                        #imp
+                    pub fn #ident(&mut self) {
+                        #body
                     }
                 }
             })
         };
         let getter = {
-            let get_fn_ident = &self.sanitized;
+            let ident = &self.sanitized;
             let return_type = &self.return_type;
-            self.getter_impl().map_or(quote!(), |imp| {
+            self.getter_impl().map_or(quote!(), |body| {
                 quote! {
-                    pub fn #get_fn_ident(&self) -> #return_type {
-                        #imp
+                    pub fn #ident(&self) -> #return_type {
+                        #body
                     }
                 }
             })
