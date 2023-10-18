b077863c98ff14446c135f500bd3f9f54a54d7bb
raviqqe <raviqqe@users.noreply.github.com>
2023-10-18 13:15:29 +0000

    deploy: c7a78469fb63174953fc424a7b2f22879dbae058

--- implementors/core/hash/trait.Hasher.js
+++ implementors/core/hash/trait.Hasher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"adler":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"adler/struct.Adler32.html\" title=\"struct adler::Adler32\">Adler32</a>"]],
-"crc32fast":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"crc32fast/struct.Hasher.html\" title=\"struct crc32fast::Hasher\">Hasher</a>"]],
-"fnv":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"fnv/struct.FnvHasher.html\" title=\"struct fnv::FnvHasher\">FnvHasher</a>"]]
+"adler":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"adler/struct.Adler32.html\" title=\"struct adler::Adler32\">Adler32</a>"]],
+"crc32fast":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"crc32fast/struct.Hasher.html\" title=\"struct crc32fast::Hasher\">Hasher</a>"]],
+"fnv":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"fnv/struct.FnvHasher.html\" title=\"struct fnv::FnvHasher\">FnvHasher</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
