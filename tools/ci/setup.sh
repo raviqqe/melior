211ae1128a8c4e6dee16e38857b746c289ced509
Yota Toyama <raviqqe@gmail.com>
2024-02-16 22:39:50 +0900

    Fix

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-packages='sys-devel/llvm dev-util/rustup'
-
 emerge-webrsync
-emerge --autounmask --autounmask-write -g $packages || dispatch-conf
-emerge -g $packages
+emerge -g sys-devel/llvm dev-lang/rust
 
 rustup default stable
