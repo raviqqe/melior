3e274ab2c24bbd3dc455cc4ce80b5a1102d618ff
Yota Toyama <raviqqe@gmail.com>
2024-02-24 18:02:32 +0900

    Mutable regions

--- melior/src/ir/region.rs
+++ melior/src/ir/region.rs
@@ -40,7 +40,11 @@ impl<'c> Region<'c> {
     }
 
     /// Inserts a block after another block.
-    pub fn insert_block_after(&self, one: BlockRef<'c, '_>, other: Block<'c>) -> BlockRef<'c, '_> {
+    pub fn insert_block_after(
+        &mut self,
+        one: BlockRef<'c, '_>,
+        other: Block<'c>,
+    ) -> BlockRef<'c, '_> {
         unsafe {
             let r#ref = BlockRef::from_raw(other.to_raw());
 
@@ -51,7 +55,11 @@ impl<'c> Region<'c> {
     }
 
     /// Inserts a block before another block.
-    pub fn insert_block_before(&self, one: BlockRef<'c, '_>, other: Block<'c>) -> BlockRef<'c, '_> {
+    pub fn insert_block_before(
+        &mut self,
+        one: BlockRef<'c, '_>,
+        other: Block<'c>,
+    ) -> BlockRef<'c, '_> {
         unsafe {
             let r#ref = BlockRef::from_raw(other.to_raw());
 
@@ -62,7 +70,7 @@ impl<'c> Region<'c> {
     }
 
     /// Appends a block.
-    pub fn append_block(&self, block: Block<'c>) -> BlockRef<'c, '_> {
+    pub fn append_block(&mut self, block: Block<'c>) -> BlockRef<'c, '_> {
         unsafe {
             let r#ref = BlockRef::from_raw(block.to_raw());
 
