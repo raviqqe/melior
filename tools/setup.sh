11e38264419ee3b5a6e22e3d24944a96b9cc9597
Yota Toyama <raviqqe@gmail.com>
2024-04-25 22:07:49 +0900

    Fix

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 llvm_version=17
 
@@ -9,3 +9,5 @@ brew install llvm@$llvm_version
 echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
 pkg-config --libs libzstd
 ls -l /opt/homebrew/opt/zstd/lib
+llvm-config --ldflags
+llvm-config --system-libs
