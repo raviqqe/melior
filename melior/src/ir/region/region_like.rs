d741a4a126fdff8dc16171742a120baa6c71a120
Yota Toyama <raviqqe@gmail.com>
2025-01-28 21:36:02 +0900

    Fix

--- melior/src/ir/region/region_like.rs
+++ melior/src/ir/region/region_like.rs
@@ -5,7 +5,7 @@ use mlir_sys::{
 };
 
 /// A region-like trait.
-pub trait RegionLike<'c, 'a>: Copy {
+pub trait RegionLike<'c: 'a, 'a>: Copy {
     /// Converts a region into a raw object.
     fn to_raw(self) -> MlirRegion;
 
